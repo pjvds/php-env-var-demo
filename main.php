1eef137b3adb09cf15c9c40d18945b3cdfab1e5b
Pieter Joost van de Sande <pj@born2code.net>
2013-11-06 19:16:44 +0100

    Fix

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 echo("Hello!\n");
-echo("Here are the defails of the MySQL service:\n";
+echo("Here are the defails of the MySQL service:\n");
 echo("\thost:\t" . $_ENV["WERCKER_MYSQL_HOST"]);
 echo("\tdb:\t" . $_ENV["WERCKER_MYSQL_DATABASE"]);
 echo("\tuser:\t" . $_ENV["WERCKER_MYSQL_USERNAME"]);
